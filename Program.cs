4fc2af6499138a60740912af9ac34ebc5c1bcb0f
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-11-04 11:20:42 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticeGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
